8723bddf27cea966da8564cbfeec9e2a710fcd02
denis <denis.baklykov@boxberry.ru>
2023-07-26 21:12:52 +0300

    галки в списке на день

--- src/components/DayView.vue
+++ src/components/DayView.vue
@@ -6,6 +6,7 @@
     <ul class="list-group list-group-flush">
       <li class="list-group-item" v-for="oneTask in taskDay">
         <div class="taskContainer">
+            <input type="checkbox" v-model="oneTask.isCompleted" @change="storeTask.updateLocalStorage"/>
           <div>{{ oneTask.time }}</div>
           <div>{{ oneTask.description }}</div>
         </div>
@@ -42,6 +43,6 @@ h5 {
 }
 .taskContainer {
   display: grid;
-  grid-template-columns: 1fr 3fr;
+  grid-template-columns: 1fr 1fr 3fr;
 }
 </style>
