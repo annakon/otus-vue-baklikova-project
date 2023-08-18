dbeb36aaaea64bdc1640b9a3f40125bfd27bab62
denis <denis.baklykov@boxberry.ru>
2023-08-18 18:17:23 +0300

    тест дней недели

--- src/components/__tests__/HomeView.spec.js
+++ src/components/__tests__/HomeView.spec.js
@@ -1,17 +1,26 @@
-import {describe, it, expect, vi} from 'vitest';
+import {describe, it, expect, vi, beforeEach} from 'vitest';
 
-import { shallowMount} from '@vue/test-utils';
-import HomeView from "@/components/HomeView.vue";
+import { mount} from '@vue/test-utils';
+import component from "@/components/HomeView.vue";
 import {createPinia, setActivePinia} from "pinia";
 
 vi.mock('vue-router')
 describe('Home', () => {
-  it('renders properly', () => {
+  let wrapper
+
+  beforeEach(() => {
     setActivePinia(createPinia())
-    const wrapper = shallowMount(HomeView,{
-      global: {
+    wrapper = mount(component, { global: {
         stubs: ['RouterLink'],
-      },});
+      } });
+  })
+  it('contains year', () => {
     expect(wrapper.text()).toContain((new Date()).getFullYear());
   });
+  ['ПН','ВТ','СР','ЧТ','ПТ','СБ','ВС'].forEach((item) => {
+    it(`contains ${item}`, () => {
+      expect(wrapper.text()).toContain(item);
+    });
+  });
+
 });
