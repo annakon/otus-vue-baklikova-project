92d44ae583d8c8dba919731e18c7437135c73c47
denis <denis.baklykov@boxberry.ru>
2023-08-27 10:03:19 +0300

    тест addTask

--- src/components/__tests__/addTask.spec.js
+++ src/components/__tests__/addTask.spec.js
@@ -4,7 +4,7 @@ import component from "@/components/addTask.vue";
 import { setActivePinia, createPinia } from 'pinia'
 
 vi.mock('vue-router')
-describe('Home', () => {
+describe('addTask', () => {
     let wrapper
     const newdate = new Date("01.08.2023")
 
@@ -18,5 +18,13 @@ describe('Home', () => {
     it('renders properly', () => {
         expect(wrapper).toBeDefined();
         expect(wrapper.exists()).toBe(true);
+    });
+    it("renders all", () => {
+        expect(wrapper.html()).toMatchSnapshot();
+    });
+    it.skip("contains correct date", () => {
+        const inputControl = wrapper.find('[id="date"]');
+        expect(inputControl.exists()).toBe(true);
+        expect(inputControl.value).toBe("01.08.2023");
     });
 });
